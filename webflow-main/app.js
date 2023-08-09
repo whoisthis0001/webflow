87dda9d15c19ca02e469c7eea242f52be89f2234
whoisthis0001 <141813362+whoisthis0001@users.noreply.github.com>
2023-08-09 12:24:13 +0400

    Add files via upload

---
+++ webflow-main/app.js
@@ -0,0 +1,16 @@
+"use strict";
+$(document).ready(function () {
+
+    $('.btn').click(function (e) {
+        e.preventDefault();
+        if (!$.trim($('input').val()) == '') {
+            let a = b => {
+                for (let i = 0; i < b; i++) { $('.minibox').append(`<h2>${b}</h2>`) };
+            }
+            let v = f => Number(f) ? a(f) : $('.minibox').append(`<h2>${f}</h2>`);
+            v($('input').val());
+        }
+        $('input').val('')
+    })
+
+});
